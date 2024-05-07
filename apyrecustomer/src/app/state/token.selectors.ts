08b188786496a52c8f695ef947ddfff93d8f653c
Mike <mmedvedev@agra.software>
2024-05-07 17:35:53 -0400

    deleted cache from ignored files

--- apyrecustomer/src/app/state/token.selectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { TokenState } from './token.reducer';
-
-export const selectTokenState = createFeatureSelector<TokenState>('token');
-
-export const selectAccessToken = createSelector(selectTokenState, (state) => state.accessToken);
-export const selectRefreshToken = createSelector(selectTokenState, (state) => state.refreshToken);
